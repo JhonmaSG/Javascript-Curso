d99a96fb86c0b9f60c3fabd321bde1a25511a140
JhonmaSG <jmserranog@udistrital.edu.co>
2024-11-24 17:02:33 -0500

    Mode strict

---
+++ ModeStrict/Strict.js
@@ -0,0 +1,14 @@
+//"use strict";
+//Modo estricto para usar variables que no se han inicializado
+x = 10;
+console.log(x);
+
+miFuncion();
+
+function miFuncion(){
+    //"use strict"
+    //tanto al inicio del programa o dentro de una 
+    //funcion en especifico
+    y = 15;
+    console.log(y);
+}
