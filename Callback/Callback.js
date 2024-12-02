129a3fddde33f887abd8d743227f6abad815c518
JhonmaSG <jmserranog@udistrital.edu.co>
2024-12-01 20:18:45 -0500

    Functions CallBack

---
+++ Callback/Callback.js
@@ -0,0 +1,22 @@
+function miFuncion1(){
+    console.log('Funcion 1');
+}
+
+function miFuncion2(){
+    console.log('Funcion 2');
+}
+
+miFuncion1();
+miFuncion2();
+
+//Función de tipo Callback
+function imprimir(mensaje){
+    console.log(mensaje);
+}
+
+function sumar(op1, op2, funcionCallBack){
+    let res = op1 + op2;
+    funcionCallBack(`Resultado: ${res}`);
+}
+
+sumar(5, 3, imprimir);
