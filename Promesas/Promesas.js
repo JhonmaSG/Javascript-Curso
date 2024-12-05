523874d8b83146bb30acea1a9c858994ea84c67c
JhonmaSG <jmserranog@udistrital.edu.co>
2024-12-05 17:31:18 -0500

    async and await

--- Promesas/Promesas.js
+++ Promesas/Promesas.js
@@ -12,6 +12,49 @@ let miPromesa = new Promise((resolved, rejected) => {
 );
 */
 
+/*
 miPromesa
 .then(valor => console.log(valor))
 .catch(error => console.log(error));
+*/
+
+//Promises with setTimeout
+let promesa = new Promise((resolved) => {
+    //console.log('Inicio promesa');
+    setTimeout( ()=> resolved('Saludos con promesa y tiemout'), 3000 );
+    //console.log('Fin promesa');
+});
+
+//promesa.then(valor => console.log(valor));
+
+//Async indica que una finción regresa una promesa
+async function miFuncionConPromesa(){
+    return 'Saludos con promesa y async';
+}
+
+//miFuncionConPromesa().then(valor => console.log(valor));
+
+
+//async con await: espera el resultado de una promesa
+
+async function funcionConPromesaYAwait(){
+    let miPromesa = new Promise( resolved => {
+        resolved('Promesa con await');
+    });
+    console.log(await miPromesa);
+}
+
+//funcionConPromesaYAwait();
+
+//Ejercicio de promesas, await, asyncs y setTomeout
+
+async function functionConPromesaAwaitTimeout(){
+    console.log('Inicio función');
+    let miPromesa = new Promise(resolved => {
+        setTimeout(()=> resolved('Promesa con await y TiemOut'),3000);
+    });
+    console.log(await miPromesa);
+    console.log('Fin función');
+}
+
+functionConPromesaAwaitTimeout();
