85ad92f0d6002ab6aef6b4b3276399ae3937b881
JhonmaSG <jmserrnaog@udistrital.edu.co>
2024-12-23 12:31:56 -0500

    added Retos de ejercicio

---
+++ Ejercicios/reto_2.js
@@ -0,0 +1,24 @@
+/*
+Santa Claus 🎅 quiere enmarcar los nombres de los niños buenos para decorar su taller 🖼️, pero el marco debe cumplir unas reglas específicas. Tu tarea es ayudar a los elfos a generar este marco mágico.
+
+Reglas:
+
+Dado un array de nombres, debes crear un marco rectangular que los contenga a todos.
+Cada nombre debe estar en una línea, alineado a la izquierda.
+El marco está construido con * y tiene un borde de una línea de ancho.
+La anchura del marco se adapta automáticamente al nombre más largo más un margen de 1 espacio a cada lado.
+*/
+
+function createFrame(names) {
+    let resul = ""
+    const maxLength = Math.max(...names.map(name => name.length));
+    const border = '*'.repeat(maxLength + 4)
+    resul = `${border}\n`
+    names.forEach(element => {
+        resul += `* ${element.padEnd(maxLength, ' ')} *\n`
+    });
+    resul += `${border}`
+    return resul
+  }
+
+  console.log(createFrame(['midu', 'madeval', 'educalvolpz']))
